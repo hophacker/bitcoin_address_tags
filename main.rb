657fb323eb39edd7cd24e33ccf1dc5ec74ae7606
Jie Feng <jokerfeng2010@gmail.com>
2014-07-17 13:12:08 -0700

    Implemented first version of crawler for crawling profile page bitcointalk.org

---
+++ main.rb
@@ -0,0 +1,91 @@
+require 'nokogiri'
+require 'open-uri'
+require 'thread'
+require 'set'
+
+
+
+class MaiWriter
+  def initialize(filename, mode)
+    @file = File.open(filename, mode)
+    @mutex = Mutex.new
+  end
+  def write(str)
+    @mutex.synchronize{
+      @file.puts str
+      @file.flush
+    }
+  end
+end
+
+def crawl_user(id_finished, address_out, id_finished_out, thread_num, total_thread, total_user_id)
+  (0..total_user_id/total_thread).each do |i|
+    user_id = total_thread * i + thread_num
+    if id_finished.include? user_id
+      puts "user_id #{user_id} finished before"
+      next
+    end
+    p "user_id #{user_id} start"
+    begin
+      doc = Nokogiri::HTML(open("https://bitcointalk.org/index.php?action=profile;u=#{user_id}"))
+    rescue OpenURI::HTTPError => error
+      sleep 10 + Random.rand(total_thread)
+      p error.io
+      redo
+    end
+    name = ""
+    bitcoin_addr = ""
+    doc.css('#bodyarea>table>tr>td>table>tr:nth-child(2)>td:nth-child(1)>table>tr').each do |ele|
+      type = ele.css('td:first-child').text
+      value = ele.css('td:nth-child(2)').text
+      if type.include? 'Name:'
+        name = value
+      end
+      if type.include? 'Bitcoin address:'
+        bitcoin_addr = value
+      end
+    end
+
+    p "user_id #{user_id} end"
+
+    if name != "" and bitcoin_addr != ""
+      str = "#{user_id},#{name},#{bitcoin_addr}"
+      puts str
+      address_out.write(str)
+    end
+    id_finished_out.write(user_id)
+  end
+end
+
+total_thread = 5
+total_user_id = 1000000
+thread_pool = []
+address_out = MaiWriter.new("address.out", "w")
+
+id_finished = Set.new
+id_finished_filename = "id_finished.out"
+
+if File::exist?(id_finished_filename)
+  File.open(id_finished_filename, "r") do |file|
+    ids = file.readlines
+    ids.each do |id|
+      id_finished.add(id.to_i)
+    end
+    file.close
+  end
+end
+
+
+id_finished_out = MaiWriter.new(id_finished_filename, "a")
+
+(1..total_thread).each do |thread_num|
+  thread_pool[thread_num] = Thread.new{
+    crawl_user(id_finished, address_out, id_finished_out, thread_num, total_thread, total_user_id)
+  }
+end
+
+thread_pool.each do |t|
+  if t
+    t.join
+  end
+end
---
+++ temp.rb
@@ -0,0 +1,13 @@
+
+
+####
+# Search for nodes by xpath
+#doc.xpath('//h3/a').each do |link|
+#  puts link.content
+#end
+
+####
+# Or mix and match.
+#doc.search('h3.r a.l', '//h3/a').each do |link|
+#  puts link.content
+#end
